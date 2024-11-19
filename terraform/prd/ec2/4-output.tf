5ab625c74596f7f6666ae338bc99cdf36b903074
Denis Gromov <path2finder@gmail.com>
2024-11-20 01:45:46 +0200

    fix

--- terraform/prd/ec2/4-output.tf
+++ terraform/prd/ec2/4-output.tf
@@ -7,3 +7,8 @@ output "ec2_devops_gha_runner_sg_id" {
   value       = aws_security_group.gha_sg.id
   description = "Github actions runner security group ID"
 }
+
+output "ec2_devops_gha_runner_role_arn" {
+  value       = aws_iam_role.ec2_ssm_role.arn
+  description = "EC2 instance role arn"
+}
