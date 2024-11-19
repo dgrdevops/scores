e508e8503bc4e1ee5dd57d97b6a146d5bcf1985c
Denis Gromov <path2finder@gmail.com>
2024-11-20 01:52:06 +0200

    cleanup

--- terraform/prd/route53/2-zone.tf
+++ terraform/prd/route53/2-zone.tf
@@ -2,8 +2,3 @@ resource "aws_route53_zone" "scores_xyz" {
   name    = "scores.xyz"
   comment = "Public hosted zone for scores.xyz"
 }
-
-resource "aws_route53_zone" "dgrdevops" {
-  name    = "dgrdevops.com"
-  comment = "Public hosted zone for dgrdevops.com"
-}
